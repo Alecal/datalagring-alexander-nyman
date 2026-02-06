dbd1dfe0608eddd4717ef17193667e189fcf6497
Alexander Nyman <stalander@gmail.com>
2026-02-06 13:20:24 +0100

    Add project files.

---
+++ Membler.Domain/Models/CreateMemberDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Membler.Domain.Models;
+
+public sealed record CreateMemberDto(
+    string FirstName,
+    string LastName,
+    string Email,
+    bool IsInstructor
+);
---
+++ Membler.Domain/Models/Member.cs
@@ -0,0 +1,17 @@
+﻿namespace Membler.Domain.Models;
+
+public class Member
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public bool IsInstructor { get; set; }
+}
+
+public class PhoneNumber
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Number { get; set; } = string.Empty;
+}
